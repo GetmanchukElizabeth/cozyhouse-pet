e3e574e92eec6736ff3a9fa8b659e4e7ed99c0d0
elizabad <elizaveta.getmanchuk@duxteam.com>
2020-03-09 23:49:24 +0200

    create about

--- dist/js/main.js
+++ dist/js/main.js
@@ -108,6 +108,17 @@
 
 /***/ }),
 
+/***/ "./src/blocks/modules/our-friends/our-friends.js":
+/*!*******************************************************!*\
+  !*** ./src/blocks/modules/our-friends/our-friends.js ***!
+  \*******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
 /***/ "./src/js/import/modules.js":
 /*!**********************************!*\
   !*** ./src/js/import/modules.js ***!
@@ -119,8 +130,11 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_header_header__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! %modules%/header/header */ "./src/blocks/modules/header/header.js");
 /* harmony import */ var _modules_header_header__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_modules_header_header__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _modules_footer_footer__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! %modules%/footer/footer */ "./src/blocks/modules/footer/footer.js");
-/* harmony import */ var _modules_footer_footer__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_modules_footer_footer__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _modules_our_friends_our_friends__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! %modules%/our-friends/our-friends */ "./src/blocks/modules/our-friends/our-friends.js");
+/* harmony import */ var _modules_our_friends_our_friends__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_modules_our_friends_our_friends__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _modules_footer_footer__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! %modules%/footer/footer */ "./src/blocks/modules/footer/footer.js");
+/* harmony import */ var _modules_footer_footer__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_modules_footer_footer__WEBPACK_IMPORTED_MODULE_2__);
+
 
 
 
--- gulp-tasks/sprites.js
+++ gulp-tasks/sprites.js
@@ -9,9 +9,7 @@ import browsersync from "browser-sync";
 gulp.task("sprites", () => {
     return gulp.src(paths.sprites.src)
         .pipe(svg({
-            shape: {
-                dest: "intermediate-svg"
-            },
+         
             mode: {
                 stack: {
                     sprite: "../sprite.svg"
